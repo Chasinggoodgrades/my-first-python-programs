34fcdd0521b3fc5a7d6da979bbd90defad473b1b
Chasinggoodgrades <31477538+Chasinggoodgrades@users.noreply.github.com>
2017-08-30 09:13:39 -0400

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,21 @@
+#this program is a hello thingy
+#
+#Chase Poland
+#August, 25th, 2017
+
+print("Hello.")
+print("What is your name?")
+name = input()
+print("It is good to meet you, " + name + ".")
+print("What is your age?")
+age = input()
+if age == "17":
+    print ("Chase is also 17!")
+else:
+    print ("Wow i'm " + age + " too!")
+print ("Where are you from?")
+location = input()
+print (location + " is a nice place.")
+
+
+
---
+++ madlib.py
@@ -0,0 +1,25 @@
+###
+#Chase Poland
+#Madlib Program
+#August 28th, 2017
+###
+print("Choose a noun. ")
+noun1 = input()
+print("Choose another noun! ")
+noun2 = input()
+print("Choose a verb!")
+verb1 = input()
+print ("Choose a second verb!")
+verb2 = input()
+print ("Choose a third verb!")
+verb3 = input()
+print ("Choose an adjective yayayay!")
+adj1 = input()
+print ("Now pick your place to dine!")
+loc = input()
+print ("Now here is a story just for you.")
+print (" ")
+print ("Your " + noun1 + " Is no longer a " + noun1 + " but it is a " + noun2)
+print ("Now your " + noun2 + " is actually going to " + verb1 + ", " + verb2 + " and " + verb3)
+print ("You are also " + adj1 + " in " + loc)
+
