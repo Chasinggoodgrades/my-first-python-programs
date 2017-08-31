61a31b7049b5d0f75ffabc75c4a15ec1262a24d3
HA i forgot my name <31477538+Chasinggoodgrades@users.noreply.github.com>
2017-08-31 09:33:52 -0400

    Add files via upload

---
+++ math_demo.py
@@ -0,0 +1,21 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #This program demonstraates how to use the math module.
+>>> #
+>>> #Chase Poland
+>>> #August 22, 2017
+>>> 
+>>> import math
+>>> 
+>>> #the math module has constants
+>>> print(math.pi)
+3.141592653589793
+>>> print(math.e)
+2.718281828459045
+>>> 
+>>> #the math module also has built in functions
+>>> print(math.sqrt(2))
+1.4142135623730951
+>>> print(math.sin(0.5))
+0.479425538604203
+>>> 
