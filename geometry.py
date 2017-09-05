9362fc45104d5b9bb503d3c6d8f2e0b37704bc67
HA i forgot my name <31477538+Chasinggoodgrades@users.noreply.github.com>
2017-09-05 08:47:41 -0400

    Add files via upload

--- geometry.py
+++ geometry.py
@@ -17,6 +17,55 @@ print(triangle_area(4,9))
 print(circle_area(5))
 print(circle_area(12))
 
+def parallelogram_area(b, h):
+        a = b * h
+        return a
+
+def trapezoid_area(a, b, h):
+        a = (a + b / 2) * h
+        return a
+
+def rectangle_volume(w, h, l):
+        a = w * h * l
+        return a
+
+def cone_volume(r, h):
+        a = math.pi * r**2 * (h/3)
+        return a
+
+def sphere_volume(r):
+        a = (4/3) * math.pi * r**3
+        return a
+
+def rectangle_sa(w, l, h):
+        a = 2*((w * l)+(h * l)+(h * w))
+        return a
+
+def sphere_sa(r):
+        a = 4*(math.pi * r**2)
+        return a
+
+def triangle_hypo(a, b):
+        a = a**2 + b**2
+        return a
+
+#function calls
+print(parallelogram_area(5,5))
+print(trapezoid_area(3,4,5))
+print(rectangle_volume(2,2,2))
+print(cone_volume(2,4))
+print(sphere_volume(4))
+print(rectangle_sa(2,3,4))
+print(sphere_sa(2))
+print(triangle_hypo(4,4))
+
+#challenge
+#def herons_formula(a, b, c):
+        #s = a + b + c
+        #d = **(1/2)(s*(s - a)*(s - b)*(s - c)
+        #return d
+#print(herons_formula(5,10,15))
+                   
 
 
 
