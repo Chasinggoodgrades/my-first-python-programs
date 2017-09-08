7faf2a11ed2764a2a96278dec0316e13faad34a7
HA i forgot my name <31477538+Chasinggoodgrades@users.noreply.github.com>
2017-09-08 08:47:18 -0400

    Add files via upload

---
+++ Fav Color.py
@@ -0,0 +1,4 @@
+print("What is your favorite color?")
+fav_color = input()
+print("So you're saying that your favorite color is " + fav_color)
+print("You have selected " + fav_color + " as your favorite color.")
---
+++ Intro-Programming.py
@@ -0,0 +1,4 @@
+print ("The knights who say, \"Ni!\"")
+birth ="2000"
+name = "Chase Poland,"
+print (name + " was born in " + birth)
